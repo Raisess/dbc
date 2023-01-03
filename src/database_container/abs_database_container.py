ab002b13035a10bd1d54490fc148c19440474651
Raisess <raisechif@gmail.com>
2023-01-03 09:31:58 -0300

    feat: create `AbstractDatabaseContainer` and `MySqlDatabaseContainer` implementation using `DockerContainer`

---
+++ src/database_container/abs_database_container.py
@@ -0,0 +1,36 @@
+import os
+
+from container.abs_container import AbstractContainer
+
+class DatabaseConnectionOpts:
+  def __init__(
+    self,
+    database: str | None,
+    host: str | None,
+    user: str | None,
+    password: str | None,
+    port: int | None
+  ):
+    self.database = os.getenv("DB_NAME") or database
+    self.host = os.getenv("DB_HOST") or host
+    self.user = os.getenv("DB_USER") or user
+    self.password = os.getenv("DB_PASS") or password
+    self.port = os.getenv("DB_PORT") or port
+
+
+class AbstractDatabaseServerContainer:
+  def __init__(self, container: AbstractContainer, connection_opts: DatabaseConnectionOpts):
+    self.__container = container
+    self.__connection_opts = connection_opts
+
+  def create(self) -> None:
+    self.__container.create(self._enviroment(self.__connection_opts))
+
+  def connect(self) -> None:
+    self.__container.execute(self._connect_command(self.__connection_opts))
+
+  def _connect_command() -> str:
+    raise NotImplemented()
+
+  def _enviroment(self, connection_opts: DatabaseConnectionOpts) -> list[str]:
+    raise NotImplemented()
---
+++ src/database_container/mysql_database_container.py
@@ -0,0 +1,23 @@
+from container.docker_container import DockerContainer
+from database_container.abs_database_container import AbstractDatabaseServerContainer, DatabaseConnectionOpts
+
+class DefaultMySqlConnectionOpts(DatabaseConnectionOpts):
+  def __init__(self, database: str):
+    super().__init__(database, "localhost", "root", "root", 3306)
+
+
+class MySqlServerContainer(AbstractDatabaseServerContainer):
+  def __init__(self, name: str, connection_opts: DatabaseConnectionOpts | None):
+    super().__init__(
+      DockerContainer(name, "mysql"),
+      connection_opts or DefaultMySqlConnectionOpts(name)
+    )
+
+  def _connect_command(self, _: DatabaseConnectionOpts) -> str:
+    return "mysql -p"
+
+  def _enviroment(self, connection_opts: DatabaseConnectionOpts) -> list[str]:
+    return [
+      f"MYSQL_DATABASE={connection_opts.database}",
+      f"MYSQL_ROOT_PASSWORD={connection_opts.password}"
+    ]
