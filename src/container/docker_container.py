e79fdfef3416cd95f35c752cca3c1da3f401f5a3
Raisess <raisechif@gmail.com>
2023-01-03 11:08:06 -0300

    fix: `DockerContainer`, pull database library before creating container

--- src/container/docker_container.py
+++ src/container/docker_container.py
@@ -8,7 +8,7 @@ class DockerContainer(AbstractContainer):
 
   def create(self, env: list[str]) -> None:
     credentials = " ".join(["-e " + credential.strip() for credential in env])
-    os.system(f"docker run --name {self.get_name()} {credentials} -d {self.get_instance()}")
+    os.system(f"docker pull {self.get_instance()} && docker run --name {self.get_name()} {credentials} -d {self.get_instance()}")
 
   def execute(self, command: str) -> None:
     os.system(f"docker exec -it {self.get_name()} {command}")
