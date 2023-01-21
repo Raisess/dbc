afd70f561573419fa84e2678e73b1af616641822
Raisess <raisechif@gmail.com>
2023-01-21 18:46:51 -0300

    feat: enter database container command, run `bash` inside the container

--- src/main.py
+++ src/main.py
@@ -4,7 +4,12 @@ from yacli import CLI
 
 from commands.connect_database_container_command import ConnectDatabaseContainerCommand
 from commands.create_database_container_command import CreateDatabaseContainerCommand
+from commands.enter_database_container_command import EnterDatabaseContainerCommand
 
 if __name__ == "__main__":
-  cli = CLI("dbc", [ConnectDatabaseContainerCommand(), CreateDatabaseContainerCommand()])
+  cli = CLI("dbc", [
+    ConnectDatabaseContainerCommand(),
+    CreateDatabaseContainerCommand(),
+    EnterDatabaseContainerCommand()
+  ])
   cli.handle()
