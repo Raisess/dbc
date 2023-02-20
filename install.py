43ad5fda3acdde39f2fbae526ae74d51c0f8a497
Raisess <raisechif@gmail.com>
2023-02-20 16:35:59 -0300

    chore: update `yacli` version

--- install.py
+++ install.py
@@ -3,6 +3,9 @@
 import os
 import site
 
+NOT_USE_SUDO = bool(os.getenv("NOT_USE_SUDO")) or False
+sudo = "" if NOT_USE_SUDO else "sudo"
+
 CLI_NAME = "dbc"
 
 BIN_PATH = f"/usr/local/bin/{CLI_NAME}"
@@ -25,16 +28,16 @@ if __name__ == "__main__":
     os.system("python3 -m pip install -r ./requirements.txt")
 
   if os.path.isdir(BIN_PATH):
-    os.system(f"sudo rm -rf {BIN_PATH}")
-  os.system(f"sudo cp ./bin/{CLI_NAME} {BIN_PATH}")
+    os.system(f"{sudo} rm -rf {BIN_PATH}")
+  os.system(f"{sudo} cp ./bin/{CLI_NAME} {BIN_PATH}")
 
   if os.path.isdir(LIB_PATH):
-    os.system(f"sudo rm -rf {LIB_PATH}")
-  os.system(f"sudo cp -r ./src {LIB_PATH}")
+    os.system(f"{sudo} rm -rf {LIB_PATH}")
+  os.system(f"{sudo} cp -r ./src {LIB_PATH}")
 
   if os.path.isdir("./etc"):
     if os.path.isdir(ETC_PATH):
-      os.system(f"sudo rm -rf {ETC_PATH}")
-    os.system(f"sudo cp -r ./etc {ETC_PATH}")
+      os.system(f"{sudo} rm -rf {ETC_PATH}")
+    os.system(f"{sudo} cp -r ./etc {ETC_PATH}")
 
   print("Installed successfully!")
