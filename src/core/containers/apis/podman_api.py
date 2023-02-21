dabc0dd14bbca70cb671365b41d2d899fadb6db8
Raisess <raisechif@gmail.com>
2023-02-21 14:33:55 -0300

    chore: create an `api` to wrap podman calls

---
+++ src/core/containers/apis/podman_api.py
@@ -0,0 +1,33 @@
+import json
+import subprocess
+
+class PodmanAPI:
+  @staticmethod
+  def Pull(image: str) -> None:
+    subprocess.getoutput(f"podman pull docker.io/library/{image}")
+
+  @staticmethod
+  def Run(container: str, image: str, public_port: int, port: int, env: str = "") -> None:
+    subprocess.getoutput(f"podman run --name {container} {env} --detach --publish {public_port}:{port}/tcp docker.io/library/{image}")
+
+  @staticmethod
+  def Start(container: str) -> None:
+    subprocess.getoutput(f"podman start {container}")
+
+  @staticmethod
+  def Stop(container: str) -> None:
+    subprocess.getoutput(f"podman stop {container}")
+
+  @staticmethod
+  def Eval(container: str, command: str) -> None:
+    subprocess.getoutput(f"podman exec -it {container} {command}")
+
+  @staticmethod
+  def Erase(container: str, volume: str) -> None:
+    subprocess.getoutput(f"podman rm {container}")
+    subprocess.getoutput(f"podman volume rm {volume}")
+
+  @staticmethod
+  def Dump(container: str) -> dict:
+    stdout = subprocess.getoutput(f"podman container inspect {container}")
+    return json.loads(stdout)[0]
--- src/core/containers/podman_container.py
+++ src/core/containers/podman_container.py
@@ -1,7 +1,4 @@
-import json
-import os
-import subprocess
-
+from core.containers.apis.podman_api import PodmanAPI
 from core.containers.abs_container import AbstractContainer, Image
 
 class PodmanContainer(AbstractContainer):
@@ -9,40 +6,38 @@ class PodmanContainer(AbstractContainer):
     super().__init__(name, image)
 
   def create(self, env: list[str]) -> None:
-    self.__run(f"pull docker.io/library/{self.get_image()}")
-    self.__run(f"run --name {self.get_name()} {self.__parse_env(env)} --detach --publish {self.get_port()}:{self.get_image_port()}/tcp docker.io/library/{self.get_image()}")
+    PodmanAPI.Pull(self.get_image())
+    PodmanAPI.Run(
+      self.get_name(),
+      self.get_image(),
+      self.get_port(),
+      self.get_image_port(),
+      self.__parse_env(env)
+    )
 
   def start(self) -> None:
-    self.__run(f"start {self.get_name()}")
+    PodmanAPI.Start(self.get_name())
 
   def stop(self) -> None:
-    self.__run(f"stop {self.get_name()}")
+    PodmanAPI.Stop(self.get_name())
 
   def execute(self, command: str) -> None:
-    self.__run(f"exec -it {self.get_name()} {command}")
+    PodmanAPI.Eval(self.get_name(), command)
 
   def destroy(self) -> None:
-    self.stop()
-    self.__run(f"rm {self.get_name()}")
-
     volume = self.__get_volume_name()
     if not volume:
       raise Exception("Volume not found for this container")
 
-    self.__run(f"volume rm {volume}")
-
-  def dump(self) -> dict:
-    stdout = subprocess.getoutput(f"podman container inspect {self.get_name()}")
-    return json.loads(stdout)[0]
-
-  def __run(self, command: str) -> None:
-    os.system(f"podman {command}")
+    PodmanAPI.Stop(self.get_name())
+    PodmanAPI.Erase(self.get_name(), volume)
 
   def __parse_env(self, env: list[str]) -> str:
     return " ".join(["-e " + credential.strip() for credential in env])
 
   def __get_volume_name(self) -> str | None:
-    mounts: list[dict] | None = self.dump().get("Mounts")
+    dump = PodmanAPI.Dump(self.get_name())
+    mounts: list[dict] | None = dump.get("Mounts")
     if not mounts:
       return None
     return mounts[0].get("Name")
